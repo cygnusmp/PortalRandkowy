b596070be0e0367154e8df283cf3fa9c23db1fff
Mariusz <cygnus.mp@gmail.com>
2019-12-07 18:03:17 +0100

    Logowanie z tworzeniem tokena

--- PortalRandkowy.API/Controllers/AuthController.cs
+++ PortalRandkowy.API/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using PortalRandkowy.API.Data;
 using PortalRandkowy.API.Dtos;
 using PortalRandkowy.API.Models;
@@ -11,10 +17,14 @@ namespace PortalRandkowy.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repository;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthRepository repository)
+        public AuthController(
+            IAuthRepository repository,
+            IConfiguration configuration)
         {
             _repository = repository;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -35,5 +45,36 @@ namespace PortalRandkowy.API.Controllers
             return StatusCode(201);
         }
         
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLogin)
+        {
+            var userFromRepo = await _repository.Login(userForLogin.UserName.ToLower()
+                                                       , userForLogin.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+            
+            // create Token
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(12),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {token = tokenHandler.WriteToken(token)});
+        }
     }
 }
---
+++ PortalRandkowy.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+namespace PortalRandkowy.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
